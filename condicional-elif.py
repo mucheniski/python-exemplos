5637967fd37cce60e8b5ff3b60a8ab1882e82a06
mucheniski <mucheniski@gmail.com>
2021-01-31 20:59:03 -0300

    condicional com elif

---
+++ condicional-elif.py
@@ -0,0 +1,11 @@
+x = 1
+y = 2
+
+if x == y:
+    print("numeros iguais")
+elif x < y:
+    print("x menor que y")
+elif y > x:
+    print("y é maior que x")
+else:
+    print("numeros diferentes")
--- condicional-else.py
+++ condicional-else.py
@@ -1,7 +1,7 @@
 a = 1
 b = 2
 
-if a > 2:
+if a > b:
     print("a maior que b")
 else:
     print("a é menor que b")
