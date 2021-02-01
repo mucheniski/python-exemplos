bdd43da74890739de46d65ef2f641bb3de08aaee
mucheniski <mucheniski@gmail.com>
2021-01-31 23:38:42 -0300

    Funcoes soma e multiplica

---
+++ funcoes-exemplo.py
@@ -0,0 +1,11 @@
+def soma(x, y):
+    return x + y
+
+def multiplica(x, y):
+    return x * y
+
+resultadoSoma = soma(2, 3)
+resultadoMultiplica = multiplica(2, 3)
+
+print(resultadoSoma)
+print(resultadoMultiplica)
