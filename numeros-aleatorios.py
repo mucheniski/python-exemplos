18d0251b1a46e39d2da9a4e12e7bd9b8c7552c68
mucheniski <mucheniski@gmail.com>
2021-02-01 09:07:40 -0300

    Numeros aleatorios com Random

---
+++ numeros-aleatorios.py
@@ -0,0 +1,10 @@
+import random
+
+# Gera um número aleatorio de 0 a 10
+numeroAleatorio1 = random.randint(0,10)
+print(numeroAleatorio1)
+
+# Escolhe um número aleatorio de uma lista
+lista = [3, 53, 67, 89]
+numeroAleatorio2 = random.choice(lista)
+print(numeroAleatorio2)
