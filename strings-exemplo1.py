109243b225420572f902651858f978d4cd6b15ae
mucheniski <mucheniski@gmail.com>
2021-01-31 21:25:23 -0300

    exemplo string concatenar tamanho letra parte da string

---
+++ strings-exemplo1.py
@@ -0,0 +1,14 @@
+variavelNome = "Diego"
+variavelSobrenome = "Mucheniski"
+
+concatenar = variavelNome + " " + variavelSobrenome
+print(concatenar)
+
+tamanho = len(concatenar)
+print(tamanho)
+
+letra = variavelNome[2]
+print(letra)
+
+parteDaString = variavelSobrenome[2:4]
+print(parteDaString)
